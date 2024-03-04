dea050800a4da975d79b84773403429d60a94569
Idan Morad <81802815+Aishgadol@users.noreply.github.com>
2024-03-04 18:11:39 +0200

    working on linear kernelization

--- ML-Project3/LinearKernelization.py
+++ ML-Project3/LinearKernelization.py
@@ -4,3 +4,23 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 from mpl_toolkits.mplot3d import Axes3D
+
+# Load the dictionary
+data_dict = np.load('linreg_data_2d.npy', allow_pickle=True).item()
+
+# Access the data as needed
+x_train = data_dict['x_train']
+y_train = data_dict['y_train']
+x_test = data_dict['x_test']
+y_test = data_dict['y_test']
+
+
+def kernel(xi, xj, sigma):
+  return np.exp((-(np.squre(np.norm(xi-xj))))/(2*np.squre(sigma)))
+
+def prepear_kernel_matrix(train, sigma):
+  K = np.zeros((len(train), len(train)))
+  for i in range(len(train)):
+    for j in range(len(train)):
+        K[i,j]=kernel(train[i],train[j],sigma)
+  return K
