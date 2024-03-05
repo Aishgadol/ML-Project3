7111bbafef5498e5e507106052327312c0556e01
Idan Morad <81802815+Aishgadol@users.noreply.github.com>
2024-03-05 21:24:51 +0200

    working on linear kernelization

--- ML-Project3/LinearKernelization.py
+++ ML-Project3/LinearKernelization.py
@@ -71,3 +71,51 @@ for idx, samp in enumerate(x_test):
 mse = mse / len(x_test)
 print(f'test mse is {mse}')
 
+#creating validation set with 20 samples and using train_test_split to tune better sigma and lamda
+def getBestSigmaLamda(x_train,y_train):
+    X_train_for_val,X_val, y_train_for_val,y_val=train_test_split(x_train,y_train, test_size=20, random_state=42)
+    sigmas=range(20,50)
+    lamdas=np.arange(0.01,0.3,0.01)
+    min_error=1
+    best_lamda=0
+    best_sigma=0
+    mse=0
+    best_mse=1
+    for lamda in lamdas:
+        for sigma in sigmas:
+            print(f'lamda:{lamda}, sigma:{sigma}')
+            mse=0
+            curr_train_kernel = prepear_kernel_matrix(X_train_for_val, sigma=sigma)
+            curr_alphas = get_alphas(curr_train_kernel, y_train_for_val,lamda=lamda)
+            for idx, samp in enumerate(X_train_for_val):
+                mse += (predict(curr_alphas, X_train_for_val, samp, sigma=4) - y_train_for_val[idx]) ** 2
+            mse = mse / len(X_train_for_val)
+            print(f'train mse is {mse}')
+            mse=0
+            for idx,samp in enumerate(X_val):
+                mse+=(predict(curr_alphas,X_train_for_val,samp,sigma=sigma)-y_val[idx])**2
+            mse = mse / len(X_val)
+            if(mse<best_mse):
+                best_mse=mse
+                best_lamda=lamda
+                best_sigma=sigma
+            print(f'val mse is {mse}\n')
+    print(f'best mse: {best_mse}, best sigma: {best_sigma}, best lamda: {best_lamda}')
+    return best_sigma,best_lamda
+best_sigma,best_lamda=getBestSigmaLamda(x_train,y_train)
+
+#training the model with new tuned hyperparameters:
+train_kernel = prepear_kernel_matrix(x_train, sigma=best_sigma)
+alphas = get_alphas(train_kernel, y_train,best_lamda)
+mse = 0
+for idx, samp in enumerate(x_train):
+  mse += (predict(alphas, x_train, samp, sigma=best_sigma) - y_train[idx]) ** 2
+mse = mse / len(x_train)
+print(f'train mse is {mse}')
+
+mse = 0
+for idx, samp in enumerate(x_test):
+  mse += (predict(alphas, x_train, samp, sigma=best_sigma) - y_test[idx]) ** 2
+mse = mse / len(x_test)
+print(f'test mse is {mse}')
+
